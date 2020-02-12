6fbf6c7e6d3971689a58a38d5edb5f883097b0b8
Zerglrisk <wmf1235@naver.com>
2020-02-12 17:47:17 +0900

    Add ComboBoxItems For Bind to ComboBox And Add DragMove EventTemplate.

---
+++ Winform-Custom-Controls/ComboBoxBinder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Winform_Custom_Controls.Model;
+
+namespace Winform_Custom_Controls
+{
+    public static class ComboBoxBinder
+    {
+        public static void BindComboBoxItems(this ComboBox box, ComboBoxItems items)
+        {
+            if (items == null || items.Item.Count == 0)
+            {
+                return;
+            }
+
+            box.DataSource = items.Item;
+            box.DisplayMember = "Text";
+            box.ValueMember = "Value";
+            if (box.BindingContext == null)
+            {
+                box.BindingContext = new BindingContext();
+            }
+
+            if (items.DefaultValue != null)
+            {
+                foreach (ComboBoxItem boxItem in box.Items)
+                {
+                    if (boxItem.Value != items.DefaultValue) continue;
+                    box.SelectedItem = boxItem;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Need Test
+        /// </summary>
+        /// <param name="box"></param>
+        /// <param name="item"></param>
+        public static void AddComboBoxItem(this ComboBox box, ComboBoxItem item)
+        {
+            if (item == null) return;
+            if (box.DataSource != null)
+            {
+                var items = (List<ComboBoxItem>) box.DataSource;
+                items.Add(item);
+            }
+        }
+    }
+}
---
+++ Winform-Custom-Controls/Model/ComboBoxItems.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform_Custom_Controls.Model
+{
+    public class ComboBoxItems
+    {
+        public List<ComboBoxItem> Item { get; set; }
+        public object DefaultValue { get; set; }
+
+        public ComboBoxItems(List<ComboBoxItem> item, object defaultValue)
+        {
+            Item = item;
+            this.DefaultValue = defaultValue;
+        }
+
+        public ComboBoxItems()
+        {
+            Item = new List<ComboBoxItem>();
+            DefaultValue = null;
+        }
+
+
+        /// <summary>
+        /// DataTable To ComboBoxItems. DataTable Must Have 'Text' And 'Value' Column.
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static ComboBoxItems DataTableToComboBoxItems(DataTable dataTable, object defaultValue = null)
+        {
+            if (dataTable == null)
+            {
+                throw new Exception("Data is Empty.");
+            }
+            if (!dataTable.Columns.Contains("Text") || !dataTable.Columns.Contains("Value"))
+            {
+                throw new Exception("Text or Value Columns is not exists.");
+            }
+
+            var items = new ComboBoxItems();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                items.Item.Add(new ComboBoxItem(row["Text"].ToString(),row["Value"]));
+            }
+
+            items.DefaultValue = defaultValue;
+
+            return items;
+        }
+
+        public static ComboBoxItems Clone(ComboBoxItems comboBoxItems)
+        {
+            var cloneComboBoxItems = new ComboBoxItems();
+            //comboBoxItems.Item.ForEach(item => { cloneComboBoxItems.Item.Add(new ComboBoxItem(item.Text,item.Value));});
+            cloneComboBoxItems.Item.AddRange(comboBoxItems.Item);
+            cloneComboBoxItems.DefaultValue = comboBoxItems.DefaultValue;
+
+            return cloneComboBoxItems;
+        }
+    }
+}
